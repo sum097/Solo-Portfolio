67800a433bd1fa8cc0d8225f7a68ed0af1eecd5a
sum097 <safimian2002@gmail.com>
2026-02-02 19:32:08 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import Hero from './sections/Hero'
+
+
+const App = () => {
+  return (
+    <>
+    <Hero />
+    </>
+  )
+}
+
+export default App
