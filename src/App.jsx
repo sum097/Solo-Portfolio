8217a1b5bac4c3bae7adaf51eeacdd51e518ed12
sum097 <safimian2002@gmail.com>
2026-02-04 22:24:26 -0600

    Updated Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './index.css'
 import Hero from './sections/Hero'
+import ShowcaseSection from './sections/ShowcaseSection'
+import NavBar from './components/NavBar'
+import LogoSection from './components/LogoSection'
 
 
 const App = () => {
   return (
     <>
+    <NavBar />
     <Hero />
+    <ShowcaseSection />
+    <LogoSection />
     </>
   )
 }
